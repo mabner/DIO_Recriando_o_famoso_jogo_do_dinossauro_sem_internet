030eb092d689f1bd2e540bad23bed15b56fbc8c1
Marcos Leite <m.abner@live.co.uk>
2022-05-04 23:29:21 -0300

    jump function

--- script.js
+++ script.js
@@ -2,8 +2,17 @@ const dino = document.querySelector('.dino');
 
 function handleKeyUp(event) {
 	if (event.keyCode === 32) {
-		console.log('Spacebar pressed');
+		jump();
 	}
+}
+
+function jump() {
+	let position = 0;
+	let upInterval = setInterval(() => {
+		position += 20;
+
+		dino.style.bottom = position + 'px';
+	}, 20);
 }
 
 document.addEventListener('keyup', handleKeyUp);
